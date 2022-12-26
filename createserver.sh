e1d502120d1af29d8232f9ea65b353a820cea567
Chimezie-Ogbu <ogbuchimezie17@gmail.com>
2022-12-26 17:22:59 +0100

    iam profile added

--- createserver.sh
+++ createserver.sh
@@ -2,4 +2,5 @@ aws cloudformation create-stack \
   --stack-name serverstack \
   --template-body file://myserver.yaml \
   --region=us-west-2 \
+  --capabilities CAPABILITY_IAM \
 #  --parameters file://network-parameters.json \
